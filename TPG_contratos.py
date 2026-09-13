63bb3a30615e43a481c0e03ff8e866010abe6326
erikafcf-punto <erikafcf0@gmail.com>
2026-09-13 20:40:01 -0300

    contratos de las funciones de consulta

---
+++ TPG_contratos.py
@@ -0,0 +1,47 @@
+# FUNCIONES DE CONSULTA / MENÚ 
+
+def imprimir_productos(productos: dict )-> None:
+    """
+    Recorre e imprime la informacion de cada producto almacenado en el diccionario 
+    
+    pre: Recibe un diccionario con toda la informacion de los productos
+
+    Post:Imprime en consola los datos de todos los  productos.
+  """      
+
+def ordenar_productos(productos:dict)-> None:
+    """
+    Ordena los productos por orden alfabetico 
+    pre: Recibe el diccionario con todos los productos 
+    post: Muestra en pantalla todos los productos ordenados 
+    """
+
+def total_facturado(ventas:dict)-> int:
+
+    """
+    Imprime la suma total de todas las ventas 
+    pre: Recibe el diccionario con todos los datos de las ventas
+    post: Retorna la suma de todas las ventas como un número entero 
+
+    """
+
+def prod_mas_vendido(productos:dict)-> None:
+    """
+    Muestra en pantalla el producto más vendido
+    pre: Recibe el diccionario de ventas 
+    post: no retorna nada, solo imprime el producto con mayor ventas
+    """
+
+def mostrar_menu() -> None:
+    """
+    Imprime las opciones disponibles en la consola
+    
+    pre:No recibe nada 
+    post: No retorna nada, solo muestra las opciones al usuario.
+    """
+def main()-> None:
+    """
+    Ejecuta el código principal
+    pre:No recibe nada
+    post:No retorna nada, permite usar el código
+    """
