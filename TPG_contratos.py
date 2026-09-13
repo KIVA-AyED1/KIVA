138eb332d7365ac41ba0c53c4f8a54fac4925a5e
EPGYDT <agustingallarati@gmail.com>
2026-09-13 20:54:22 -0300

    Contratos de la seccion de productos.

--- TPG_contratos.py
+++ TPG_contratos.py
@@ -1,3 +1,54 @@
+# FUNCIONES DE PRODUCTOS
+
+def registrar_producto(productos: dict) -> dict:
+    """
+    Permite registrar un producto nuevo al programa.
+    Pre: Recibe el diccionario con los datos de los productos.
+    Post: Devuelve el diccionario actualizado con los datos del nuevo producto.
+    """
+
+
+
+def eliminar_producto(productos: dict) -> dict:
+    """
+    Permite eliminar un producto en base a su nombre o id.
+    Pre: Recibe el diccionario que contiene todos los datos de todos los productos.
+    Post: Devuelve el diccionario actualizado sin los datos del producto que fue eliminado.
+    """
+
+
+
+def buscar_producto(productos: dict) -> str:
+    """
+    Permite buscar un producto por su nombre o id.
+    Pre: Recibe el diccionario que contiene los nombres y los id's.
+    Post: Retorna un string con los datos del producto que se queria buscar.
+    """
+
+
+
+def modificar_stock(productos: dict) -> dict:
+    """
+    Permite modificar el stock de un producto.
+    Pre: recibe el diccionario con los datos de todos los productos.
+    Post: devuelve el diccionario actualizado con el stock modificado.
+    """
+
+
+
+def modificar_precio(dict: dict) -> dict:
+    """
+    Permite modificar el precio de un producto.
+    Pre: Recibe el diccionario con los datos de todos los productos.
+    Post: Devuelve el diccionario con el precio actualizado.
+    """
+
+
+
+# FUNCIONES DE VENTAS
+
+
+
 # FUNCIONES DE CONSULTA / MENÚ 
 
 def imprimir_productos(productos: dict )-> None:
